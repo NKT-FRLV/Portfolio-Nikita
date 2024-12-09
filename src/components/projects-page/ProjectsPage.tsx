c64ed667d7a4e279fcc36b4b99c15a1b87e7d902
FROL <nkt.frlv7@yandex.ru>
2024-12-09 21:46:27 +0100

    project galery fixed bag

--- src/components/projects-page/ProjectsPage.tsx
+++ src/components/projects-page/ProjectsPage.tsx
@@ -1,18 +1,48 @@
 import React, { useState, useEffect } from 'react'
-import { IoChevronForward , IoChevronBack , IoButt } from 'react-icons/io5';
-import { motion, AnimatePresence} from 'framer-motion'
+import { IoChevronForward , IoChevronBack } from 'react-icons/io5';
+import { motion, AnimatePresence } from 'framer-motion'
+import { wrap } from "popmotion";
 import { useInView } from 'react-intersection-observer';
 import AnimatedLetters from '../animatedLetters/AnimatedLetters'
 import animateStyles from '../animatedLetters/animatedLetters.module.css'
 import Section from '../section/Section'
 import styles from './projects.module.css'
 import { projects } from '../../data'
+import clsx from 'clsx';
+
+const variants = {
+    enter: (direction: number) => ({
+      x: direction > 0 ? 400 : -400, // Смещение для захода карточки
+      opacity: 0,
+    }),
+    center: {
+      zIndex: 1,
+      x: 0, // Центрирование карточки
+      opacity: 1,
+    },
+    exit: (direction: number) => ({
+      zIndex: 0,
+      x: direction < 0 ? 400 : -400, // Смещение для выхода карточки
+      opacity: 0,
+    }),
+  };
 
 
 const ProjectsPage = () => {
 
     const [letterClass, setLetterClass] = useState('');
-    const [activeProject, setActiveProject] = useState(0);
+    // const [activeProject, setActiveProject] = useState(0);
+
+    // ВОЗМОЖНО ОШИБКА В ТОМ КАК МЫ ПОЛУЧАЕМ PAGE , МЫ ИСПОЛЬЗУЕМ PAGE КАК KEY И АНИМАЦИЯ ПУТАЕТСЯ
+    const [[page, direction], setPage] = useState([0, 0]);
+
+
+    const projectIndex = wrap(0, projects.length, page);
+
+    const paginate = (newDirection: number) => {
+        setPage([page + newDirection, newDirection]);
+    };
+
     const { ref: sectionRef, inView: isInView } = useInView({
         // triggerOnce: true,
         threshold: 0.6, // 100% элемента должны быть видимы
@@ -39,43 +69,56 @@ const ProjectsPage = () => {
                 <h2 className={styles.heading}>
                     <AnimatedLetters letterClass={letterClass} strArray={titleLetters} idx={1} />
                 </h2>
-                <div className={styles.list}>
-                    <div className={styles.arrowButtonStart}>
-                        <IoChevronBack style={{ width: '60px', height: '100px' }} onClick={() => setActiveProject(activeProject === 0 ? projects.length - 1 : activeProject - 1)} />
-                    </div>
-                    <div className={styles.arrowButtonEnd}>
-                        <IoChevronForward style={{ width: '60px', height: '100px' }} onClick={() => setActiveProject(activeProject === projects.length - 1 ? 0 : activeProject + 1)} />
-                    </div>
-                    <AnimatePresence>
-                        { projects.map((item, index) => 
-                             activeProject === index ? (
-                             <motion.div
-                                className={styles.project}
-                                key={item.title}
-                                initial={{ opacity: 0, x: -200 }}
-                                animate={{ opacity: 1, x: 0 }}
-                                exit={{ opacity: 0, x: -200 }}
-                            >
-                                <div className={styles.projectImageContainer}>
-                                    <img src={item.image} alt={item.title} className={styles.projectImage} />
-                                </div>
-                                <div className={styles.projectInfo}>
-                                    <h3 className={styles.projectTitle}>{item.title}</h3>
-                                    <p className={styles.projectDescription}>{item.description}</p>
-                                    <ul className={styles.tecnologies}>
-                                        {item.tecnologies.map((tag, index) => (
-                                            <li key={index} className={styles.projectTag}>{tag}</li>
-                                        ))}
-                                    </ul>
-                                </div>
-                                <div className={styles.projectLinks}>
-                                    <a target="_blank" href={item.urlGitHub}>GitHub</a>
-                                    <a target="_blank" href={item.urlDemo}>Demo</a>
-                                </div>
-                            </motion.div> ) : null
-                        )}
-                    </AnimatePresence>
+                <div className={clsx(styles.arrow, styles.arrowStart)}>
+                    <IoChevronBack style={{ width: '60px', height: '100px' }} onClick={() => paginate(-1)} />
                 </div>
+                <div className={clsx(styles.arrow, styles.arrowEnd)}>
+                    <IoChevronForward style={{ width: '60px', height: '100px' }} onClick={() => paginate(1)} />
+                </div>
+                <motion.div className={styles.list}>
+                    <AnimatePresence initial={false} custom={direction}>
+                        <motion.div
+                            className={styles.project}
+                            key={projectIndex} // Используем page как ключ для анимации
+                            custom={direction}
+                            variants={variants}
+                            initial="enter"
+                            animate="center"
+                            exit="exit"
+                            transition={{
+                                x: { type: 'spring', stiffness: 300, damping: 30 },
+                                opacity: { duration: 0.2 },
+                            }}
+                        >
+                            <div className={styles.projectImageContainer}>
+                                <img
+                                src={projects[projectIndex].image}
+                                alt={projects[projectIndex].title}
+                                className={styles.projectImage}
+                                />
+                            </div>
+                            <div className={styles.projectInfo}>
+                                <h3 className={styles.projectTitle}>{projects[projectIndex].title}</h3>
+                                <p className={styles.projectDescription}>{projects[projectIndex].description}</p>
+                                <ul className={styles.tecnologies}>
+                                {projects[projectIndex].tecnologies.map((tag, idx) => (
+                                    <li key={idx} className={styles.projectTag}>
+                                    {tag}
+                                    </li>
+                                ))}
+                                </ul>
+                            </div>
+                            <div className={styles.projectLinks}>
+                                <a target="_blank" href={projects[projectIndex].urlGitHub}>
+                                GitHub
+                                </a>
+                                <a target="_blank" href={projects[projectIndex].urlDemo}>
+                                Demo
+                                </a>
+                            </div>
+                    </motion.div>
+                </AnimatePresence>
+                </motion.div>
                 
                 
             </div>
