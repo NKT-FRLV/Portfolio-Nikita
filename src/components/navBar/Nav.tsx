56cebdf47ecfb800996185cbf6e47cd1c0cb7dc9
FROL <nkt.frlv7@yandex.ru>
2024-12-13 16:02:44 +0100

    feture: resume download

--- src/components/navBar/Nav.tsx
+++ src/components/navBar/Nav.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
-
-import { IoHome, IoMail, IoPerson, IoBriefcase   } from 'react-icons/io5';
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import { FaLinkedin, FaGithub, FaTelegram, FaInstagram, FaLink } from 'react-icons/fa';
+import { IoHome, IoMail, IoPerson, IoBriefcase } from 'react-icons/io5';
 import styles from './sidebar.module.css'
 import { useWindowSize } from '../../hooks';
 
@@ -11,10 +12,13 @@ interface Props {
 
 const Nav = ({activeSection, moveToSection}: Props) => {
 
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   const { width } = useWindowSize(); // Используем ширину экрана из хука
 
   // Определяем размер иконок в зависимости от ширины экрана
   const iconSize = width > 1050 ? 35 : width > 440 ? 30 : 25;
+  const iconSotialSize = width > 1050 ? 25 : 20;
+  const linksBlockWidth = width > 1050 ? 250 : width > 550 ? 220 : 180;
 
   return (
     
@@ -41,7 +45,7 @@ const Nav = ({activeSection, moveToSection}: Props) => {
         </a>
         <a className={styles.link} href="#" onClick={() => moveToSection('projects')}>
           <span className={styles.linkIcon}>
-            <IoBriefcase      size={iconSize} color={activeSection === 2 ? '#00ff7f' : 'inherit'}/>
+            <IoBriefcase size={iconSize} color={activeSection === 2 ? '#00ff7f' : 'inherit'}/>
           </span>
           <span className={styles.linkText}>
             Projects
@@ -56,8 +60,39 @@ const Nav = ({activeSection, moveToSection}: Props) => {
           </span>
         </a>
       </div>
-      <div className={styles.links}>
-        <a href="#">Resume</a>
+      <div className={styles.resume}>
+        <a
+          className={styles.resumeLink}
+          href='documents/resume_Nikita_Frolov.pdf'
+          download='Frolov Nikita Resume'
+        >
+          RESUME
+        </a>
+        <div className={styles.sotialsToolBar}>
+          <div className={styles.sotialsBarToogler} onClick={() => setIsOpen(!isOpen)}>
+            <FaLink size={iconSotialSize}/>
+            Sotials
+          </div>
+          <motion.div
+            className={styles.sotials}
+            
+            animate={{ opacity: isOpen ? 1 : 0.5, width: isOpen ? linksBlockWidth : 10, paddingLeft: isOpen ? 16 : 0, paddingRight: isOpen ? 16 : 0 }}
+            transition={{ duration: 0.3 }}
+          >
+            <motion.a href="https://web.telegram.org/k/#-955617383" target="_blank" style={{width: iconSize, height: iconSize}} animate={{ opacity: isOpen ? 1 : 0 }} transition={{ duration: 0.3}}>
+              <FaTelegram size={iconSize} />
+            </motion.a>
+            <motion.a href="https://www.instagram.com/nkt.frlv/" target="_blank" style={{width: iconSize, height: iconSize}} animate={{ opacity: isOpen ? 1 : 0 }} transition={{ duration: 0.3}}>
+              <FaInstagram size={iconSize} />
+            </motion.a>
+            <motion.a href="https://github.com/mi-viejo-amigo" target="_blank" style={{width: iconSize, height: iconSize}} animate={{ opacity: isOpen ? 1 : 0 }} transition={{ duration: 0.3}}>
+              <FaGithub size={iconSize} />
+            </motion.a>
+            <motion.a href="#" target="_blank" style={{width: iconSize, height: iconSize}} animate={{ opacity: isOpen ? 1 : 0 }} transition={{ duration: 0.3 }}>
+              <FaLinkedin size={iconSize} />
+            </motion.a>
+          </motion.div>
+        </div>
       </div>
     </nav>
 
