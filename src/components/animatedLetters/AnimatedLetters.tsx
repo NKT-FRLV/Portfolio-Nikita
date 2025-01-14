9c4422d8cf31c60dcd9af1b1e88e29c52db7e974
FROL <nkt.frlv7@yandex.ru>
2025-01-14 13:02:01 +0100

    feture-critical: animatedLetters component, try to realize touchPad Hover Letters animation

--- src/components/animatedLetters/AnimatedLetters.tsx
+++ src/components/animatedLetters/AnimatedLetters.tsx
@@ -1,4 +1,4 @@
-
+import { useState } from 'react';
 interface AnimatedLettersProps {
     letterClass: string;
     strArray: string[];
@@ -7,16 +7,26 @@ interface AnimatedLettersProps {
 
 const AnimatedLetters = ({ letterClass, strArray, idx }: AnimatedLettersProps) => {
   const isHoverClass = letterClass.includes('Hover');
+  const [hoveredIndices, setHoveredIndices] = useState<number[]>([]);
+
+  // Функция добавления индекса при ховере или свайпе
+  const handlePointerMove = (index: number) => {
+    if (!hoveredIndices.includes(index)) {
+      setHoveredIndices((prev) => [...prev, index]);
+    }
+  };
   return (
     <span>
       {strArray.map((char, i) => (
         <span
           key={`${char}-${i}`}
-          className={letterClass}
+          className={`${letterClass} ${hoveredIndices.includes(i) ? 'textAnimateHover' : ''}`}
           style={{
             animationDelay: isHoverClass ? '0s' : `${(i + idx) * 0.1}s`, // Убираем задержку для hover-класса
             color: char === 'N' ? '#00ff7f' : '',
           }}
+          onPointerMove={() => handlePointerMove(i)} // Срабатывает при проведении пальцем или мышью
+          onMouseEnter={() => handlePointerMove(i)}  // Для обычного ховера мыши
         >
           {char === ' ' ? '\u00A0' : char}
         </span>
